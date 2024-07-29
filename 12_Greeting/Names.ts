2747d0806713513a2ff0f7d15fedda8b7e9abb86
jawarianoor <160591831+jawarianoor@users.noreply.github.com>
2024-07-29 19:02:05 +0500

    Add files via upload

---
+++ 12_Greeting/Names.ts
@@ -0,0 +1,5 @@
+let names :string[] = [`Jawaria`, `Mahanoor`, `Nimra`, `Maryam`];
+const message :string = `Welcome to Typescript class:`
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]);
+}
