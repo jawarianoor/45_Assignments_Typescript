455e9fa07a7eb181208586b9bede4a491b8fc2b5
jawarianoor <160591831+jawarianoor@users.noreply.github.com>
2024-07-29 18:43:19 +0500

    Delete 8_Favorite_Number directory

--- 8_Favorite_Number/Favorite_Number.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const favorite_number = 9;
-const message = `Here is your favorite number ` + favorite_number;
-console.log(message);
--- 8_Favorite_Number/Favorite_Number.ts
+++
@@ -1,3 +0,0 @@
-const favorite_number :number = 9 ;
-const message :string = `Here is your favorite number ` + favorite_number ;
-console.log(message) ;
