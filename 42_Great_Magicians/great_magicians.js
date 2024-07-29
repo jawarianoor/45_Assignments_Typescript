213a83af59ec81a0379fdce80c88b8df8364bb46
jawarianoor <160591831+jawarianoor@users.noreply.github.com>
2024-07-29 20:28:12 +0500

    Add files via upload

---
+++ 42_Great_Magicians/great_magicians.js
@@ -0,0 +1,11 @@
+"use strict";
+// Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make great() that modifies the array of magicians by adding the phrase the Great to each magician's name. Call show _magicians() to see that the list has actually been modified.
+let magicianNames = ['Harry Houdini', 'Hermione Granger', 'David Copperfield', 'David Blaine'];
+function show_magicians(great) {
+    for (let item of magicianNames) {
+        console.log(great, item);
+    }
+}
+;
+show_magicians(" Hello, How are you Mr.");
+show_magicians("\n Show Me The Magic Mr.");
