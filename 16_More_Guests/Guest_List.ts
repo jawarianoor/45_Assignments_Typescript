0ef1a87a83bc2197521ad98fbf147c2524051aa0
jawarianoor <160591831+jawarianoor@users.noreply.github.com>
2024-07-29 19:11:56 +0500

    Add files via upload

---
+++ 16_More_Guests/Guest_List.ts
@@ -0,0 +1,16 @@
+let Guest_List :string[] = [`Imran Khan`, `Asif Ali Zardari`, `Nawaz Sharif`];
+//  for(let i=0; i<Guest_List.length; i++){
+//     console.log(`Dear Mr.` + Guest_List[i] + `,\n\nIt is our pleasure to invite you in our party.\n\nThanks You!\n\n`)
+// }
+let absent_Guest :string = `Imran Khan` ;
+let new_Guest :string = `Kamran Tessori` ;
+Guest_List[0] = new_Guest ;
+for(let i=0; i<Guest_List.length; i++){
+    console.log(`Dear Mr.` + Guest_List[i] + `,\n\nIt is our pleasure to invite you in our party.\n\nThanks You!\n\n`)}
+    console.log (`Mr. ${absent_Guest} is not coming to the party`);
+    console.log(`Good News! We find big table so we are inviting 3 more guests. `)
+    Guest_List.unshift(`Sir Zia Khan`);
+    Guest_List.splice(2 , 0, `Maryam Nawaz`);
+    Guest_List.push(`Bilawal Butto `);
+    for(let i=0; i<Guest_List.length; i++){
+    console.log(`Dear Mr.` + Guest_List[i] + `,\n\nIt is our pleasure to invite you in our party.\n\nThanks You!\n\n`)}
