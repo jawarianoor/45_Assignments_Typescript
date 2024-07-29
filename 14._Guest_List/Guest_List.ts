cc3ba03dbb429c2a27f255cccdc8576c0914b81c
jawarianoor <160591831+jawarianoor@users.noreply.github.com>
2024-07-29 19:06:41 +0500

    Add files via upload

---
+++ 14._Guest_List/Guest_List.ts
@@ -0,0 +1,4 @@
+let Guest_List :string[] = [`Imran Khan`, `Asif Ali Zardari`, `Nawaz Sharif`];
+for(let i=0; i<Guest_List.length; i++){
+    console.log(`Dear Mr.` + Guest_List[i] + `,\n\nIt is our pleasure to invite you in our party.\n\nThanks You!`)
+}
