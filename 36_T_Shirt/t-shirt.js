c8e42a20d9145983d39161f9229e8ece8cd536dc
jawarianoor <160591831+jawarianoor@users.noreply.github.com>
2024-07-29 20:12:51 +0500

    Add files via upload

---
+++ 36_T_Shirt/t-shirt.js
@@ -0,0 +1,9 @@
+"use strict";
+// T-shirt: Write a function called make shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it.
+// call the function.
+function make_shirt(size, text) {
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+make_shirt('large', 'Hello world');
+make_shirt('medium', 'Hello world');
+make_shirt('small', 'Hello world');
